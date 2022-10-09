bf4bff63be1f9ced2631982d6b56d8ae5ea35b69
Winston Hsieh <heretse@gmail.com>
2022-10-09 23:29:36 +0800

    assign default slot to green for the first deployment

--- blue-green/autoDeployBlueOrGreen.sh
+++ blue-green/autoDeployBlueOrGreen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-currentSlot=$(helm get values --all deploy-test | python -c 'import sys,yaml; yml = yaml.safe_load(sys.stdin); print(yml["productionSlot"]);')
+currentSlot=$((helm get values --all deploy-test) || echo 'productionSlot: green') | python -c 'import sys,yaml; yml = yaml.safe_load(sys.stdin); print(yml["productionSlot"]);')
 
 if [ $currentSlot == "blue" ]; then
   newSlot=green
@@ -17,7 +17,7 @@ echo "Start to deploy on slot: $newSlot with version: $newAppVersion"
 
 if [ $newSlot == "blue" ]; then
   kubectl delete deployments/bg-deploy-blue
-	helm upgrade deploy-test . --set blue.enabled=true --set blue.timestamp="$(date '+%Y-%m-%d %H:%M:%S')" --set blue.appVersion=$newAppVersion --reuse-values --debug
+  helm upgrade deploy-test . --set blue.enabled=true --set blue.timestamp="$(date '+%Y-%m-%d %H:%M:%S')" --set blue.appVersion=$newAppVersion --reuse-values --debug
   kubectl rollout status deployments/bg-deploy-blue
   sleep 2
   helm upgrade deploy-test . --set productionSlot=blue --reuse-values --debug
--- blue-green/deployBlueOrGreen.sh
+++ blue-green/deployBlueOrGreen.sh
@@ -17,7 +17,7 @@ fi
 
 if [ $newSlot == "blue" ]; then
   kubectl delete deployments/bg-deploy-blue
-	helm upgrade deploy-test . --set blue.enabled=true --set blue.timestamp="$(date '+%Y-%m-%d %H:%M:%S')" --set blue.appVersion=$appVersion --reuse-values --debug
+  helm upgrade deploy-test . --set blue.enabled=true --set blue.timestamp="$(date '+%Y-%m-%d %H:%M:%S')" --set blue.appVersion=$appVersion --reuse-values --debug
 elif [ $newSlot == "green" ]; then
   kubectl delete deployments/bg-deploy-green
   helm upgrade deploy-test . --set green.enabled=true --set green.timestamp="$(date '+%Y-%m-%d %H:%M:%S')" --set green.appVersion=$appVersion --reuse-values --debug
